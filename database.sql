38208384103102a7e866f3554986cd2670f446d1
Joe Thompson <smnindian30@yahoo.com>
2020-10-23 16:58:30 -0500

    set up db in pool module, added GET route in server

---
+++ database.sql
@@ -0,0 +1,4 @@
+CREATE TABLE "To-Do" (
+  "id" serial primary key,
+  "task" varchar(240)
+);
--- server/server.js
+++ server/server.js
@@ -8,6 +8,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
+// ROUTES
+
+app.get('/todo', (req, res) => {
+  const queryText = 'SELECT * FROM "To-Do";';
+
+  pool
+    .query(queryText)
+    .then((dbResponse) => {
+      console.log(dbResponse);
+      res.send(dbResponse.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
