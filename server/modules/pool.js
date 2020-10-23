69567961c6589c20b9c32c338e56d6b744b23a5b
Joe Thompson <smnindian30@yahoo.com>
2020-10-23 15:38:24 -0500

    set up pool module, need to input database still

---
+++ server/modules/pool.js
@@ -0,0 +1,21 @@
+const pg = require('pg');
+
+// DB Config Options
+const Pool = pg.Pool;
+const pool = new Pool({
+  database: '', // name of our database
+  host: 'localhost', // where is your database
+  port: 5432,
+  max: 10, // how many connections
+  idleTimeoutMillis: 30000, // 30 second timeout
+});
+
+pool.on('connect', () => {
+  console.log('Pool Connected');
+});
+
+pool.on('error', (err) => {
+  console.log('Pool Error:', err);
+});
+
+module.exports = pool;
